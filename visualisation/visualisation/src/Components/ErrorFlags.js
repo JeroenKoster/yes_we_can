dc7e60993ee390b34c5ea02f737ab3473e6d5b3a
Jeroen Koster <jeroenkoster90@gmail.com>
2019-02-01 10:39:22 +0100

    add errorflags

---
+++ visualisation/visualisation/src/Components/ErrorFlags.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import { List, ListSubheader, ListItem, ListItemIcon ,ListItemText, Paper } from '@material-ui/core';
+import ErrorOutlineRounded from '@material-ui/icons/ErrorOutlineRounded';
+import DoneOutlined from '@material-ui/icons/DoneOutlined';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4,
+  },
+});
+
+class NestedList extends React.Component {
+  state = {
+    open: true,
+  };
+
+  handleClick = () => {
+    this.setState(state => ({open: !state.open}));
+  };
+
+
+  render() {
+    const {classes, flags} = this.props;
+
+    return (
+      <div>
+        <List
+          subheader={<ListSubheader component="div">PDO1 Error Flags</ListSubheader>}
+          className={classes.root}
+        >
+          {
+            Object.keys(flags).map((key) => {
+              return (
+                <ListItem button>
+                  <ListItemIcon>
+                    { flags[key] ? (<DoneOutlined/>) : (<ErrorOutlineRounded/>) }
+                  </ListItemIcon>
+                  <ListItemText inset primary={key}/>
+                </ListItem>
+              )
+            })
+          }
+        </List>
+      </div>
+    );
+  }
+}
+
+NestedList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NestedList);
